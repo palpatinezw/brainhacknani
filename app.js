2fcfe6c008e46758a59925af40c341aaf50c0afc
flyyee <8mrgil@gmail.com>
2021-06-15 09:06:37 +0800

    Added readme and other api functionality

--- app.js
+++ app.js
@@ -40,7 +40,7 @@ app.get('/create', async (req, res) => {
                     const user = {username: req.query.username, password: hash, salt: salt}
                     const result = await users.insertOne(user);
                     res(result)
-                }                
+                }              
             } finally {
                 await mongoClient.close();
             }
@@ -75,16 +75,16 @@ app.get('/create', async (req, res) => {
     }
 })
 
-app.get('/login', async (req, res) => {
-    res_value = {
-        success: 0
-    }
-    await new Promise(async (res, err) => {
-        if (!req.query.username || !req.query.password) {
+async function auth(username, password) {
+    return new Promise(async (res, err) => {
+        let res_value = {
+            success: 0
+        }
+        if (!username || !password) {
             res_value = {
                 success: 0
             }
-            res()
+            res(res_value)
         } else {
             try {
                 mongoClient = new MongoClient(_connstring, {
@@ -94,10 +94,10 @@ app.get('/login', async (req, res) => {
                 await mongoClient.connect();
                 const database = mongoClient.db('sigma');
                 const users = database.collection('users');
-                const query = { username: req.query.username };
+                const query = { username: username };
                 const userExists = await users.findOne(query);
                 if (userExists) {
-                    const hash = crypto.pbkdf2Sync(req.query.password, userExists.salt,  
+                    const hash = crypto.pbkdf2Sync(password, userExists.salt,  
                         1000, 64, `sha512`).toString(`hex`);
                     if (hash == userExists.password) {
                         res_value = {
@@ -115,11 +115,158 @@ app.get('/login', async (req, res) => {
                 }
             } finally {
                 await mongoClient.close();
+                res(res_value)
+            }
+        }
+    })
+}
+
+app.get('/login', async (req, res) => {
+    let res_value = {
+        success: 0
+    }
+
+    await auth(req.query.username, req.query.password)
+    .then(res => res_value = res)
+
+    res.json(res_value)
+})
+
+app.get('/create_circle', async (req, res) => {
+    let res_value = {
+        success: 0
+    }
+    await new Promise(async (res, err) => {
+        if (!req.query.username || !req.query.password || !req.query.circleName
+            || !req.query.circleVis) {
+            res()
+        } else {
+            try {
+                mongoClient = new MongoClient(_connstring, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                })
+
+                await mongoClient.connect();
+                const database = mongoClient.db('sigma');
+                let userLegit = await auth(req.query.username, req.query.password)
+                userLegit = userLegit.success
+
+                if (userLegit) {
+                    const circles = database.collection('circles');
+                    const query = { name: req.query.circleName };
+                    const circleExists = await circles.findOne(query);
+
+                    if (!circleExists) {
+                        const circle = {name: req.query.circleName, vis: req.query.circleVis, members: {
+                            [req.query.username] : [0]
+                                // username: ,
+                                // flairs: [0],
+                                // visroles: ["owner"],
+                                // hidroles: ["creator", "flair0"]
+                        }, flairs: [
+                            {
+                                name: "Owner",
+                                id: 0,
+                                active: 1,
+                                power: 0,
+                                allowAssignFlairs: 1,
+                                allowCreateFlairs: 1,
+                                allowAcceptMembers: 1
+                            }
+                        ], infoText: "Hey! I just created my own circle." }
+                        const result = await circles.insertOne(circle);
+                        if (result.insertedCount == 1) {
+                            res_value.success = 1
+                        }
+                    }
+                }             
+            } finally {
+                await mongoClient.close();
                 res()
             }
         }
     })
-    res.json(res_value)
+    .then(result => {
+        res.json(res_value)
+    })
+    .catch(result => {
+        res.json({
+            success: 0
+        })
+    })
+    
+})
+
+app.get('/create_flair_info', async (req, res) => {
+    let res_value = {
+        success: 0
+    }
+    await new Promise(async (res, err) => {
+        if (!req.query.username || !req.query.password || !req.query.circleName) {
+            res()
+        } else {
+            try {
+                mongoClient = new MongoClient(_connstring, {
+                    useNewUrlParser: true,
+                    useUnifiedTopology: true,
+                })
+
+                await mongoClient.connect();
+                const database = mongoClient.db('sigma');
+                let userLegit = await auth(req.query.username, req.query.password)
+                userLegit = userLegit.success
+
+                if (userLegit) {
+                    const circles = database.collection('circles');
+                    const query = { name: req.query.circleName };
+                    const circle = await circles.findOne(query);
+                    console.log(circle)
+                    if (circle) {
+                        if (req.query.username in circle.members) {
+                            let userflairs = circle.members[req.query.username]
+                            let minFlairPower = -1
+                            let minFlair
+                            for (flairId in userflairs) {
+                                let flair = circle.flairs[flairId]
+                                if (flair.allowCreateFlairs) {
+                                    if (minFlairPower == -1) {
+                                        minFlairPower = flair.power
+                                        minFlair = flair
+                                    } else {
+                                        minFlairPower = Math.min(flair.power, minFlairPower)
+                                        minFlair = flair
+                                    }
+                                }
+                            }
+                            if (minFlairPower != -1) {
+                                // can create flairs
+                                res_value = {
+                                    success: 1,
+                                    power: minFlair.power,
+                                    allowAssignFlairs: minFlair.allowAssignFlairs,
+                                    allowCreateFlairs: minFlair.allowCreateFlairs,
+                                    allowAcceptMembers: minFlair.allowAcceptMembers
+                                }
+                            }
+                        }
+                    }
+                }             
+            } finally {
+                await mongoClient.close();
+                res()
+            }
+        }
+    })
+    .then(result => {
+        res.json(res_value)
+    })
+    .catch(result => {
+        res.json({
+            success: 0
+        })
+    })
+    
 })
 
 app.listen(port, () => {
---
+++ readme.md
@@ -0,0 +1,58 @@
+# host: https://flyyee-brainhackserver.herokuapp.com/
+
+## example implementation with fetch:
+`fetch('https://flyyee-brainhackserver.herokuapp.com/create?username=bob&password=harambe');`
+
+# API endpoints:
+
+**`/create`**
+*access method: GET with query params*
+username: username
+password: password in plaintext form
+
+*returns:*
+JSON Object with success key set to 0/1 for failure/success, eg.
+`{ success: 1, info: "created user" }`
+
+
+
+**`/login`**
+
+*access method: GET with query params*
+username: username
+password: password in plaintext form
+
+*returns:*
+JSON Object with success key set to 0/1 for failure/success
+
+
+
+**`/create_circle`**
+*access method: GET with query params*
+*info: This endpoint creates a circle with the current user as the only member, with full admin privs*
+username: username
+password: password in plaintext form
+circleName: name of the circle
+circleVis: visibility of the circle (currently unused)
+
+*returns:*
+JSON Object with success key set to 0/1 for failure/success
+
+
+
+**`/create_flair_info`**
+*access method: GET with query params*
+
+username: username
+password: password in plaintext form
+circleName: name of the circle
+
+returns: where power is an integer >= 0
+`{
+	success: 1,
+	power: 0-9,
+	allowAssignFlairs: 0/1,
+	allowCreateFlairs: 0/1,
+	allowAcceptMembers: 0/1
+}` OR 
+`{ success: 0 }` for all failures (unauth, no flairs, no flairs allowed to be assigned etc)
