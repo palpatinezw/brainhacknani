8b41302483e7575ad6f71382080f973807439f2e
michaelchen-lab <michaelchenkaijie2004@gmail.com>
2021-06-16 20:19:55 +0800

    style ProtectedProfile.js

--- components/ProtectedProfile.js
+++ components/ProtectedProfile.js
@@ -1,6 +1,5 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { View, Text, TouchableOpacity, KeyboardAvoidingView, TextInput, Keyboard, Pressable } from "react-native";
-import {Circle} from 'react-native-shape'
 import tailwind from 'tailwind-rn'
 import styles from '../styles/styles'
 import Ionicons from 'react-native-vector-icons/Ionicons'
@@ -8,50 +7,40 @@ import Ionicons from 'react-native-vector-icons/Ionicons'
 const ProtectedProfile = ({ route, navigation }) => {
     const { username, password } = route.params
 
-   
+    const [isEditing, setIsEditing] = useState(false)
+
     return (
-        <View style={tailwind('flex-col px-4 py-4')}>
-            
-            <View style = {tailwind('flex flex-row items-center justify-center')}>
-               <Ionicons name='person-circle' size={70} color='black'/>
-                <View style={tailwind('flex flex-column')}>
-                    <TextInput style = {tailwind('text-gray-800 text-xl font-medium border-radius')} placeholder = {"Enter your name"} value = {null}></TextInput>
-                    <Text style = {tailwind('text-center text-gray-800 text-xl')}>{username}</Text>
+        <View style={tailwind('px-6 py-4')}>
+
+            <View style = {tailwind('flex flex-row items-center')}>
+               <Ionicons name='person-circle' size={120} color='#6B7280'/>
+                <View style={tailwind('flex flex-col flex-grow')}>
+                    <TextInput style = {tailwind('text-gray-800 text-lg border-b-2 pb-1 w-full')} placeholder={"Your name..."} onFocus={() => setIsEditing(true)}></TextInput>
+                    <Text style = {tailwind('text-gray-800 text-sm')}>@{username}</Text>
+                    { isEditing ? (
+                        <Pressable style={tailwind('mt-1 py-0.5 bg-blue-500 rounded')} onPress={() => {Keyboard.dismiss(); setIsEditing(false)}}>
+                            <Text style={tailwind('text-center text-white text-sm')}>Save</Text>
+                        </Pressable>
+                    ) : (null)}
                 </View>
-                
+
             </View>
-            
-            <TouchableOpacity onPress = {() => {
-                Keyboard.dismiss()
-            }}>
-                <Text style = {tailwind('text-xl mt-3 font-bold border-4 border-black border-opacity-100')}>Save Profile</Text>
-            </TouchableOpacity>
-            
-            <View style = {tailwind('flex items-left justify-left mt-12')}>
-                
-                <Text style = {tailwind('text-xl pb-2')} > About me </Text>
-                
-                <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
-                    <TextInput style = {tailwind('border-2 rounded-lg p-3') /* add some styling here */} placeholder = {'A short bio of less than 150 words'} value = {null} multiline = {true} >
-                    </TextInput> 
-                </KeyboardAvoidingView>
-                
+
+            <Text style = {tailwind('text-xl font-bold')}>About Me</Text>
+            <TextInput style = {tailwind('border-2 rounded-lg px-3 py-2 mt-4') } placeholder={'A short bio...'} minHeight={120} textAlignVertical={'top'} multiline={true} onFocus={() => setIsEditing(true)}>
+            </TextInput>
+
+            <Text style = {tailwind('text-xl font-bold mt-6')}>Socials</Text>
+            <View style = {tailwind('flex flex-row items-center mt-4')}>
+                <Text style = {tailwind('w-20')}>Instagram</Text>
+                <TextInput style = {tailwind('flex-grow ml-4 py-0.5 px-2 border-2 border-gray-500 rounded')} placeholder = {'Insert URL...'} onFocus={() => setIsEditing(true)}>
+                </TextInput>
             </View>
-            <View style = {tailwind('flex-row mt-10 items-center')}>
-                <Text style = {tailwind('pr-28 text-xl')}> Instagram </Text>
-                <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
-                    <TextInput style = {tailwind('border-2 rounded-lg p-3') /*add some styling here */} placeholder = {'Insert social links here'} value = {null} multiline = {true}> 
-                    </TextInput>
-                </KeyboardAvoidingView>
+            <View style = {tailwind('flex flex-row items-center mt-2')}>
+                <Text style = {tailwind('w-20')}>Facebook</Text>
+                <TextInput style = {tailwind('flex-grow ml-4 py-0.5 px-2 border-2 border-gray-500 rounded')} placeholder = {'Insert URL...'} onFocus={() => setIsEditing(true)}>
+                </TextInput>
             </View>
-            <View style = {tailwind('flex-row mt-10 items-center')}>
-                <Text style = {tailwind('pr-28 text-xl')}> Facebook </Text>
-                <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
-                    <TextInput style = {tailwind('border-2 rounded-lg p-3') /*add some styling here */} placeholder = {'Insert social links here'} value = {null} multiline = {true}> 
-                    </TextInput>
-                </KeyboardAvoidingView>
-            </View>
-            
         </View>
     );
 }
