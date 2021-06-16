dbc8e763741693ae0a1c27c647f9e2b90b7e2cc2
danielcwq <dancwq2@gmail.com>
2021-06-16 09:28:50 +0800

    Added some text fields

--- components/ProtectedProfile.js
+++ components/ProtectedProfile.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View, Text } from "react-native";
+import { View, Text, TouchableOpacity, KeyboardAvoidingView, TextInput, Keyboard } from "react-native";
 import tailwind from 'tailwind-rn'
 
 const ProtectedProfile = ({ route, navigation }) => {
@@ -7,7 +7,23 @@ const ProtectedProfile = ({ route, navigation }) => {
 
     return (
         <View style={tailwind('px-4 py-4')}>
-            <Text>Profile</Text>
+            <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
+                <TextInput style = {tailwind('text-gray-800 text-xl font-medium mt-4 text-right border-radius')} placeholder = {"Enter your name"} value = {null}>
+                </TextInput>
+            </KeyboardAvoidingView>
+            <Text style = {tailwind('text-right text-gray-800 text-xl pt-4')}>{username}</Text>
+            <View style = {null/*need to add styling here */}>
+                <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
+                    <TextInput style = {tailwind('') /* add some styling here */} placeholder = {'A short bio of less than 150 words'} value = {null}>
+                    </TextInput> 
+                </KeyboardAvoidingView>
+            </View>
+            <View style = {null /* add some styling here again */}>
+                <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
+                    <TextInput style = {tailwind('') /*add some styling here */} placeholder = {'Insert social links here'} value = {null}> 
+                    </TextInput>
+                </KeyboardAvoidingView>
+            </View>
         </View>
     );
 }
