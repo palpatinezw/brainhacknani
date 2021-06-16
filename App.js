05dd5a5f5b7ca60f41220c5640048839eafb8585
michaelchen-lab <michaelchenkaijie2004@gmail.com>
2021-06-16 19:18:26 +0800

    revert back to 9b9542

--- App.js
+++ App.js
@@ -1,7 +1,9 @@
-import React from "react";
+import { setStatusBarBackgroundColor, StatusBar } from "expo-status-bar";
+import React, { useState, useEffect } from "react";
 import { createStackNavigator } from "@react-navigation/stack";
 import { NavigationContainer } from "@react-navigation/native";
 
+import Home from './components/Home'
 import Login from './components/Login'
 import Register from './components/Register'
 
@@ -11,9 +13,13 @@ const Stack = createStackNavigator()
 
 export default function App() {
 
+  // const [username,setUsername] = useState()
+  // const [password, setPassword] = useState()
+
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="login">
+      <Stack.Navigator initialRouteName="home">
+        <Stack.Screen name='home' component = {Home} options={{ title: 'Login/Register' }}/>
         <Stack.Screen name='login' component = {Login} options={{ title: 'Login' }}/>
         <Stack.Screen name='register' component = {Register} options={{ title: 'Register' }}/>
 
@@ -22,3 +28,10 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+// <Stack.Screen name='login'>
+//     {() => <Login {...{setUsername, setPassword}} options={{ title: 'Login' }}/>}
+// </Stack.Screen>
+// <Stack.Screen name='register'>
+//     {() => <Register {...{setUsername, setPassword}} options={{ title: 'Register' }}/>}
+// </Stack.Screen>
--- components/Login.js
+++ components/Login.js
@@ -1,33 +1,32 @@
+import { setStatusBarBackgroundColor, StatusBar } from "expo-status-bar";
 import React, { useState, useEffect } from "react";
 import {
-    Text, View, Button, TouchableOpacity, KeyboardAvoidingView, TextInput, Keyboard, Pressable
+    Text, View, Button, TouchableOpacity, KeyboardAvoidingView, TextInput, Keyboard,
 } from "react-native";
-import Spinner from 'react-native-loading-spinner-overlay';
-import { CommonActions } from '@react-navigation/native';
+import { CommonActions } from '@react-navigation/native'
 
-import tailwind from 'tailwind-rn'
+import styles from '../styles/styles'
 
 const Login = ({ navigation }) => {
 
-  const [password, setPassword] = useState()
-  const [username, setUsername] = useState()
-  const [isLoading, setIsLoading] = useState()
+  const [password, setPassword] = useState();
+  const [username, setUsername] = useState();
 
   function handleLogin(){
 
-    setIsLoading(true)
     Keyboard.dismiss()
     fetch('https:/flyyee-brainhackserver.herokuapp.com/login?username='+username+'&password='+password)
     .then(response => response.json())
     .then(data => {
         console.log(username)
         console.log(password)
-        setIsLoading(false)
         if (data.success === 1) {
-            navigation.dispatch(CommonActions.reset({
+            navigation.dispatch(
+              CommonActions.reset({
                 index: 0,
                 routes: [{ name: 'ProtectedHome', params:{username, password}}],
-            }))
+              })
+            );
         } else {
             console.log(false)
         }
@@ -35,32 +34,25 @@ const Login = ({ navigation }) => {
   }
 
   return (
-    <View style={tailwind('flex justify-center items-center h-full')}>
-      <Text style={tailwind('pt-2 pb-1 text-gray-700 text-4xl font-bold text-blue-700 border-t-4 border-b-4 border-blue-300')}>Σіgма</Text>
+    <View>
+      <KeyboardAvoidingView behavior = {Platform.OS === 'ios' ? 'padding': 'height'}>
+        <TextInput style = {styles.input} placeholder = {'Enter your username'} value = {null}
+        onChangeText ={(text) => setUsername(text)}
+        />
+      </KeyboardAvoidingView>
 
-      <Text style={tailwind('mt-8 text-gray-700 text-left w-64')}>Username</Text>
-      <TextInput style={tailwind('px-2 py-1 mt-2 border-2 border-gray-500 w-64 rounded')} value = {null}  placeholder={'Enter your username'}
-        onChangeText ={(text) => setUsername(text)} />
+      <KeyboardAvoidingView
+        behavior = {Platform.OS === 'ios' ? 'padding': 'height'}
+       >
+      <TextInput secureTextEntry={true} style={styles.input} placeholder={'Enter your password'} value = {null}
+      onChangeText ={(text) => setPassword(text)}
+      />
+      <Button
+        title="Login"
+        onPress={() => handleLogin()}
+      />
 
-      <Text style={tailwind('mt-2 text-gray-700 text-left w-64')}>Password</Text>
-      <TextInput secureTextEntry={true} style={tailwind('px-2 py-1 mt-2 border-2 border-gray-500 w-64 rounded')} placeholder={'Enter your password'} value = {null}
-      onChangeText ={(text) => setPassword(text)} />
-
-      <View style={tailwind('flex flex-row mt-8 w-64')}>
-          <Pressable style={tailwind('flex-grow py-1 bg-blue-500 border-2 border-blue-500 rounded')} onPress={() => handleLogin()}>
-              <Text style={tailwind('text-center text-white')}>Login</Text>
-          </Pressable>
-          <Pressable
-            style={tailwind('flex-grow ml-2 py-1 bg-white border-2 border-blue-500 rounded')}
-            onPress={() => {
-                navigation.dispatch(CommonActions.reset({index: 0, routes: [{ name: 'register'}],}))
-            }}
-          >
-              <Text style={tailwind('text-center text-blue-500')}>Register</Text>
-          </Pressable>
-          <Spinner visible={isLoading} textContent='Loading' textStyle={tailwind('text-white text-sm')}/>
-      </View>
-
+      </KeyboardAvoidingView>
     </View>
   )
 }
