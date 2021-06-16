d19dcde28d936c070f452306de178ef7798ace6e
Jayanthi <jayanthi@Kannans-MacBook-Air.local>
2021-06-16 15:57:54 +0800

    explore

--- components/ProtectedExplore.js
+++ components/ProtectedExplore.js
@@ -1,11 +1,10 @@
 import { setStatusBarNetworkActivityIndicatorVisible } from 'expo-status-bar';
 import React, {useState, useEffect} from 'react'
-import { View, Text, TextInput, TouchableOpacity, Keyboard, Button, Pressable } from "react-native";
+import { View, Text, TextInput, TouchableOpacity, Keyboard, Button, Pressable,FlatList } from "react-native";
 import { createStackNavigator } from "@react-navigation/stack"
 import { NavigationContainer } from '@react-navigation/native';
 import tailwind from 'tailwind-rn';
 import styles from '../styles/styles'
-import Protected from './Protected';
 import { Ionicons } from "@expo/vector-icons";
 
 
@@ -14,12 +13,12 @@ const ProtectedExplore = ({ route, navigation }) => {
     const { username, password } = route.params;
     const [search, setSearch]=useState();
     const [textvalue, settextvalue]=useState("Reccomended");
-    const [results, setresults]=useState("");
+    const [results, setresults]=useState();
     const [reccommended,setreccommended]=useState(
-        <TouchableOpacity onPress={ () => navigation.navigate("Description", 
-        { }) 
+        <TouchableOpacity onPress={ () => navigation.navigate("Description",
+        { })
         }>
-    
+
         <Text>Video Games</Text>
           </TouchableOpacity>
 
@@ -28,27 +27,29 @@ const ProtectedExplore = ({ route, navigation }) => {
     //   navigation.setOptions({
     //     headerRight: () => (
     //       <TouchableOpacity onPress={addNote}>
-    //         <Ionicons
+    //       <Ionicons
     //           name="ios-create-outline"
     //           size={30}
     //           color="black"
     //           style={{
-    //             color: "#f55",
-    //             marginRight: 10,
-    //           }}
-    //         />
-    //       </TouchableOpacity>
-    //     ),
+    //           color: "#f55",
+    //              marginRight: 10,
+    //            }}
+    //          />
+    //        </TouchableOpacity>
+    //  ),
 
-    
-      
+
+
     //   });
     // });
+
+
 
     function search2(){
       
 
-      fetch(`http://flyyee-brainhackserver.herokuapp.com/search_circles?username=`+username+`&password=`+password+`&searchstring=`+search)
+    fetch(`http://flyyee-brainhackserver.herokuapp.com/search_circles?username=`+username+`&password=`+password+`&searchstring=`+search)
    .then(response => response.json())
     .then(data => {
         console.log(search);
@@ -56,9 +57,9 @@ const ProtectedExplore = ({ route, navigation }) => {
         console.log (data);
         
         if (data.success === 1) {
-          console.log(true)
+         
 
-          console.log(response.data)
+          console.log(data.results);
           
           setresults(data.results); 
             // navigation.dispatch(CommonActions.reset({
@@ -71,6 +72,21 @@ const ProtectedExplore = ({ route, navigation }) => {
     })
   }
 
+  function renderCircles( {item} ) {
+    return (
+        <View style={tailwind('h-15 rounded-lg px-2 flex-row border-2')}>
+           <TouchableOpacity onPress={ () => navigation.navigate("Description",
+        { })
+        }>
+           <Text>{item}</Text>
+           </TouchableOpacity>
+        </View>
+    )
+}
+function separator() {
+    return (<View style={{height:5}}></View>)
+}
+
     return (
         <View style={tailwind('px-4 py-4')}>
             <Text>Explore</Text>
@@ -78,24 +94,24 @@ const ProtectedExplore = ({ route, navigation }) => {
         onChangeText ={(text) => setSearch(text)} onFocus={search1}/>
         <Text>{textvalue}</Text>
         <Text>{reccommended}</Text>
+        <Button onPress={search2} title="enter" />
         <FlatList data={results} renderItem={renderCircles} ItemSeparatorComponent={separator} keyExtractor={(item, index) => index.toString()}/>
-        <Button onPress={search2()} title="enter" />
         </View>
             );
-        
-    
+
+
         function search1(){
-    
+
             settextvalue("Search Results")
             setreccommended("")
             }
-            
-    
-      
+
+
+
         }
- 
-    
-      
+
+
+
 
 
     function ProtectedJoinCommunity( ) {
@@ -107,12 +123,12 @@ const ProtectedExplore = ({ route, navigation }) => {
         )
       }
 
-  
+
 
 const Stack = createStackNavigator()
 
 export default function ProtectedExploreStack({route}){
-  let {username, password} = route.params 
+  let {username, password} = route.params
   return (
     <Stack.Navigator>
     <Stack.Screen name="Explore" component={ProtectedExplore} initialParams={{username, password}} />
@@ -123,7 +139,7 @@ export default function ProtectedExploreStack({route}){
 
 // export default function ProtectedExploreStack({route}){
 //     return (
-  
+
 //         <Stack.Navigator>
 //           <Stack.Screen name="Explore" component={ProtectedExplore} initialParams={{username, password}} />
 //           <Stack.Screen name="Description" component={Description} initialParams={{username, password}} />
--- components/ProtectedProfile.js
+++ components/ProtectedProfile.js
@@ -1,32 +1,57 @@
 import React from 'react'
-import { View, Text, TouchableOpacity, KeyboardAvoidingView, TextInput, Keyboard } from "react-native";
+import { View, Text, TouchableOpacity, KeyboardAvoidingView, TextInput, Keyboard, Pressable } from "react-native";
 import {Circle} from 'react-native-shape'
 import tailwind from 'tailwind-rn'
 import styles from '../styles/styles'
+import Ionicons from 'react-native-vector-icons/Ionicons'
 
 const ProtectedProfile = ({ route, navigation }) => {
     const { username, password } = route.params
 
+   
     return (
-        <View style={tailwind('flex px-4 py-4')}>
-            <Circle color = 'grey' scale = {1.5} style = {styles.profilepic}/>
-            <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
-                <TextInput style = {tailwind('text-gray-800 text-xl font-medium text-right border-radius')} placeholder = {"Enter your name"} value = {null}>
-                </TextInput>
-            </KeyboardAvoidingView>
-            <Text style = {tailwind('text-right text-gray-800 text-xl pt-4')}>{username}</Text>
-            <View style = {tailwind('flex items-center justify-center mt-24')}>
+        <View style={tailwind('flex-col px-4 py-4')}>
+            
+            <View style = {tailwind('flex flex-row items-center justify-center')}>
+               <Ionicons name='person-circle' size={70} color='black'/>
+                <View style={tailwind('flex flex-column')}>
+                    <TextInput style = {tailwind('text-gray-800 text-xl font-medium border-radius')} placeholder = {"Enter your name"} value = {null}></TextInput>
+                    <Text style = {tailwind('text-center text-gray-800 text-xl')}>{username}</Text>
+                </View>
+                
+            </View>
+            
+            <TouchableOpacity onPress = {() => {
+                Keyboard.dismiss()
+            }}>
+                <Text style = {tailwind('text-xl mt-3 font-bold border-4 border-black border-opacity-100')}>Save Profile</Text>
+            </TouchableOpacity>
+            
+            <View style = {tailwind('flex items-left justify-left mt-12')}>
+                
+                <Text style = {tailwind('text-xl pb-2')} > About me </Text>
+                
                 <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
-                    <TextInput style = {tailwind('border-2 rounded-lg p-3') /* add some styling here */} placeholder = {'A short bio of less than 150 words'} value = {null} multiline = {true}>
+                    <TextInput style = {tailwind('border-2 rounded-lg p-3') /* add some styling here */} placeholder = {'A short bio of less than 150 words'} value = {null} multiline = {true} >
                     </TextInput> 
                 </KeyboardAvoidingView>
+                
             </View>
-            <View style = {tailwind('flex justify-center items-center mt-10')}>
+            <View style = {tailwind('flex-row mt-10 items-center')}>
+                <Text style = {tailwind('pr-28 text-xl')}> Instagram </Text>
                 <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
                     <TextInput style = {tailwind('border-2 rounded-lg p-3') /*add some styling here */} placeholder = {'Insert social links here'} value = {null} multiline = {true}> 
                     </TextInput>
                 </KeyboardAvoidingView>
             </View>
+            <View style = {tailwind('flex-row mt-10 items-center')}>
+                <Text style = {tailwind('pr-28 text-xl')}> Facebook </Text>
+                <KeyboardAvoidingView behaviour = {Platform.OS === 'ios' ? 'padding': 'height'}>
+                    <TextInput style = {tailwind('border-2 rounded-lg p-3') /*add some styling here */} placeholder = {'Insert social links here'} value = {null} multiline = {true}> 
+                    </TextInput>
+                </KeyboardAvoidingView>
+            </View>
+            
         </View>
     );
 }
